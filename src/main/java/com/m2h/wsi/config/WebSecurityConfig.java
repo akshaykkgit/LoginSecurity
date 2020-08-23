72763369b18bdca3d01d280a9a68294e1ed0c53d
akshay <akshay@M2HJ33.NEXTGBL.COM>
2020-08-23 17:24:59 +0530

    CSRF amd dropdown

--- src/main/java/com/m2h/wsi/config/WebSecurityConfig.java
+++ src/main/java/com/m2h/wsi/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
  
-        http.csrf().disable();
+      //  http.csrf().disable();
  
         // The pages does not require login
         http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
